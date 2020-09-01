027ed772f97dae0137a528434aa71ff3aee1832a
Daniel Valcarce <davamix@gmail.com>
2020-09-01 12:14:48 +0200

    Added Microsoft.NET.Test.SDK in order to run tests from Text Explorer. Added test for OrderedDesc and OrderedLength options

---
+++ 202008/OrderService/OrderService.API.Tests/Services/Ordered/OrderedDesc/Sort.cs
@@ -0,0 +1,28 @@
+﻿using OrderService.API.Services.Ordered;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace OrderService.API.Tests.Services.Ordered.OrderedDesc
+{
+    public class Sort
+    {
+        IOrderedType _orderedType;
+        string _text => "My car is red";
+        IEnumerable<string> _exepected => new List<string> { "red", "My", "is", "car"  };
+
+        public Sort()
+        {
+            _orderedType = new OrderedFactory().GetOrdered(OrderType.AlphabeticDesc);
+        }
+
+        [Fact]
+        public void TextIsSortedInAscendingMode()
+        {
+            var result = _orderedType.Sort(_text);
+
+            Assert.Equal(_exepected, result);
+        }
+    }
+}
